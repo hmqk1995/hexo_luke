ac37d64aefbf5ede96fab20bb36035f73fae4016
钱锟 <qiankun@wepiao.com>
2016-08-01 02:07:49 +0800

    topscroll

--- themes/luke/source/js/scripts.js
+++ themes/luke/source/js/scripts.js
@@ -1,1 +1,32 @@
-// Empty JS for your own code to be here
+// 向下滚动顶部固定标题通栏
+$(function(){
+
+  // offset: 偏移修正
+  function topFixer(selector, offset) {
+    var $document = $(document),
+        $selector = $(selector),
+        // 选择元素距离页面顶部的高
+        $selector_top = $selector.offset().top;
+    $document.scroll(function(){
+      console.log($(this).scrollTop(), $selector_top);
+      if ($(this).scrollTop() > $selector_top + offset) {
+        $selector.css({
+          'position': 'fixed',
+          'top': (0 - offset) + 'px',
+          'z-index': 999,
+          'opacity': 0.8
+        });
+      } else {
+        $selector.css({
+          'position': '',
+          'top': '',
+          'z-index': '',
+          'opacity': ''
+        });
+      }
+    });
+  }
+
+  topFixer('.heading', 180);
+
+});
