6fdcff233e66f540782376fc4d17c1607af35596
钱锟 <qiankun@wepiao.com>
2016-08-01 02:28:32 +0800

    scroll all but img shaking

--- themes/luke/source/js/scripts.js
+++ themes/luke/source/js/scripts.js
@@ -6,20 +6,30 @@ $(function(){
     var $document = $(document),
         $selector = $(selector),
         // 选择元素距离页面顶部的高
-        $selector_top = $selector.offset().top;
+        $selector_top = $selector.offset().top,
+        $selector_left = $selector.offset().left,
+        $selector_width = $selector.width(),
+        $selector_height = $selector.height(),
+        offset = offset || 0;
     $document.scroll(function(){
       console.log($(this).scrollTop(), $selector_top);
       if ($(this).scrollTop() > $selector_top + offset) {
         $selector.css({
+          'width': $selector_width + 'px',
+          'height': $selector_height + 'px',
           'position': 'fixed',
           'top': (0 - offset) + 'px',
+          'left': $selector_left + 'px',
           'z-index': 999,
           'opacity': 0.8
         });
       } else {
         $selector.css({
+          'width': '',
+          'height': '',
           'position': '',
           'top': '',
+          'left': '',
           'z-index': '',
           'opacity': ''
         });
@@ -28,5 +38,6 @@ $(function(){
   }
 
   topFixer('.heading', 180);
+  topFixer('#nameboxarea');
 
 });
